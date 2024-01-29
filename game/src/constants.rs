0e62318490a297be98b40a8478b5c8ead268df0c
Jacob Torrång <kopatheonlyone@hotmail.com>
2024-01-29 23:12:46 +0100

    Add back doors in proper position

--- game/src/constants.rs
+++ game/src/constants.rs
@@ -1,24 +1,25 @@
 use bevy::prelude::*;
 
-pub const PLAYER_SIZE: Vec2 = Vec2::new(64.0, 64.0);
+pub const PLAYER_SIZE: Vec2 = Vec2::new(128.0, 128.0);
 pub const PLAYER_SPEED: f32 = 500.0;
 
 pub const WIDTH: f32 = 1920.;
 pub const HEIGHT: f32 = 1080.;
 
-pub const FLOOR_WIDTH: f32 = 640.;
-pub const FLOOR_HEIGHT: f32 = 480.;
+pub const WALL_THICKNESS: f32 = 120.0;
 
-pub const WALL_THICKNESS: f32 = 320.0;
+pub const FLOOR_WIDTH: f32 = 1280. - WALL_THICKNESS * 2.;
+pub const FLOOR_HEIGHT: f32 = 1040. - WALL_THICKNESS * 2.;
+
 pub const LEFT_WALL: f32 = -FLOOR_WIDTH / 2. - WALL_THICKNESS;
 pub const RIGHT_WALL: f32 = FLOOR_WIDTH / 2. + WALL_THICKNESS;
 pub const BOTTOM_WALL: f32 = -FLOOR_HEIGHT / 2. - WALL_THICKNESS;
 pub const TOP_WALL: f32 = FLOOR_HEIGHT / 2. + WALL_THICKNESS;
 
-pub const WALL_HEIGHT: f32 = FLOOR_HEIGHT / 2. + WALL_THICKNESS - DOOR_WIDTH;
-pub const WALL_WIDTH: f32 = FLOOR_WIDTH / 2. + WALL_THICKNESS - DOOR_WIDTH;
+pub const WALL_HEIGHT: f32 = FLOOR_HEIGHT / 2. + WALL_THICKNESS;
+pub const WALL_WIDTH: f32 = FLOOR_WIDTH / 2. + WALL_THICKNESS;
 
-pub const DOOR_WIDTH: f32 = 100.;
+pub const DOOR_WIDTH: f32 = 120.;
 pub const DOOR_THICKNESS: f32 = 2.;
 
 pub const WALL_COLOR: Color = Color::rgb(0.01, 0.01, 0.01);
--- game/src/doors.rs
+++ game/src/doors.rs
@@ -1,5 +1,7 @@
-use crate::constants;
-use bevy::prelude::*;
+use crate::constants::{
+    self, BOTTOM_WALL, DOOR_WIDTH, LEFT_WALL, RIGHT_WALL, TOP_WALL, WALL_THICKNESS,
+};
+use bevy::{prelude::*, sprite::Anchor};
 
 pub enum DoorLocation {
     Left,
@@ -18,32 +20,34 @@ pub struct CollisionEvent;
 
 impl DoorLocation {
     fn position(&self) -> Vec2 {
-        match self {
-            DoorLocation::Left => Vec2::new(-100., 0.),
-            DoorLocation::Right => Vec2::new(100., 0.),
-            DoorLocation::Bottom => Vec2::new(0., 100.),
-            DoorLocation::Top => Vec2::new(0., -100.),
-        }
+        let wall_position = match self {
+            DoorLocation::Left => Vec2::new(LEFT_WALL, 0.),
+            DoorLocation::Right => Vec2::new(RIGHT_WALL, 0.),
+            DoorLocation::Bottom => Vec2::new(0., BOTTOM_WALL),
+            DoorLocation::Top => Vec2::new(0., TOP_WALL),
+        };
+        wall_position
     }
-    fn size(&self) -> Vec2 {
+
+    fn anchor(&self) -> Anchor {
         match self {
-            DoorLocation::Right => Vec2::new(100., 100.),
-            DoorLocation::Left => Vec2::new(100., 100.),
-            DoorLocation::Top => Vec2::new(100., 100.),
-            DoorLocation::Bottom => Vec2::new(100., 100.),
+            DoorLocation::Left => Anchor::CenterLeft,
+            DoorLocation::Right => Anchor::CenterRight,
+            DoorLocation::Bottom => Anchor::BottomCenter,
+            DoorLocation::Top => Anchor::TopCenter,
         }
     }
 }
 
 impl DoorBundle {
     pub fn new(location: DoorLocation) -> DoorBundle {
-        let size = location.size();
         DoorBundle {
             sprite_bundle: SpriteBundle {
                 transform: Transform::from_translation(location.position().extend(2.)),
                 sprite: Sprite {
+                    anchor: location.anchor(),
                     color: constants::DOOR_COLOR,
-                    custom_size: Some(size.clone()),
+                    custom_size: Some(Vec2::splat(DOOR_WIDTH)),
                     ..default()
                 },
                 ..default()
